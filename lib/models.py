3b9c14b62d39a362677abba266c460619bb81a23
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-10-01 17:20:13 +0300

    Completed Assignmnet

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,30 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, unique=True, nullable=False)
+    genre = Column(String, nullable=False)
+    platform = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+
+    # Define the one-to-many relationship between Game and Review
+    reviews = relationship('Review', backref='game', lazy='dynamic')
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    score = Column(Integer, nullable=False)
+    comment = Column(String, nullable=True)
+    
+    # Define the foreign key to establish the relationship
+    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
+
+    def __init__(self, score, comment, game_id):
+        self.score = score
+        self.comment = comment
+        self.game_id = game_id
